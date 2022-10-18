3a830bc090a410630ef114f27a0953b9abe721cf
Rafael Silva <rafael.silva@esg.ipsantarem.pt>
2022-10-18 23:12:54 +0100

    Demo

--- Aula1/src/Demo_GUI.java
+++ Aula1/src/Demo_GUI.java
@@ -2,6 +2,12 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 public class Demo_GUI {
 
 	private JFrame frame;
@@ -34,8 +40,58 @@ public class Demo_GUI {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 530, 369);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnInicio = new JButton("INICIO");
+		btnInicio.setForeground(new Color(255, 255, 255));
+		btnInicio.setBackground(new Color(0,0,255));
+		btnInicio.setBounds(20, 80, 80, 80);
+		frame.getContentPane().add(btnInicio, BorderLayout.WEST);
+		
+		JButton btnMeio = new JButton("MEIO");
+		btnMeio.setForeground(new Color(255, 255, 255));
+		btnMeio.setBackground(new Color(255, 0, 0));
+		btnMeio.setBounds(200, 80, 80, 80);
+		frame.getContentPane().add(btnMeio);
+		
+		JButton btnFim = new JButton("FIM");
+		btnFim.setForeground(new Color(255, 255, 255));
+		btnFim.setBackground(new Color(0, 255, 0));
+		btnFim.setBounds(400, 80, 80, 80);
+		frame.getContentPane().add(btnFim);
+		
+		JButton btnAvancar = new JButton("AVAN\u00C7AR");
+		btnAvancar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				//System.out.println("teste");
+				
+				// Avançar com o botão para a frente
+				if (btnInicio.getX() + btnInicio.getWidth() < btnMeio.getX()) {
+					btnInicio.setLocation(btnInicio.getX() + 20, btnInicio.getY());
+				} else if (btnInicio.getX() + btnInicio.getWidth() == btnMeio.getX() &&
+						btnInicio.getY() + btnInicio.getHeight() > btnMeio.getY()) {
+					// Subir o botão
+					btnInicio.setLocation(btnInicio.getX(), btnInicio.getY() - 20);
+				} else if (btnInicio.getY() + btnInicio.getHeight() == btnMeio.getY()
+						&& btnInicio.getX() < btnMeio.getX() + btnMeio.getWidth()){
+					// Avançar botão
+					btnInicio.setLocation(btnInicio.getX() + 20, btnInicio.getY());
+				} else if (btnInicio.getX() == btnMeio.getX() + btnMeio.getWidth()
+						&& btnInicio.getY() < btnMeio.getY()) {
+					// Descer
+					btnInicio.setLocation(btnInicio.getX(), btnInicio.getY() + 20);
+				} else if (btnInicio.getX() + btnInicio.getWidth() < btnFim.getX()) {
+					btnInicio.setLocation(btnInicio.getX() + 20, btnInicio.getY());
+				}
 
+				
+				
+			}
+		});
+		btnAvancar.setBounds(172, 261, 173, 40);
+		frame.getContentPane().add(btnAvancar);
+	}
 }
