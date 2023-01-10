e572b8bbe8e23aef7ed8a5cd13e39ac54b0fa63b
Rafael Silva <rafael.silva@esg.ipsantarem.pt>
2023-01-10 21:54:09 +0000

    Ficha9_911

---
+++ Ficha9_911/src/main.java
@@ -0,0 +1,52 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		// Declara um vector
+		int[] sequencia = {33, 2, 4, 52, 10};
+		
+		for (int i = sequencia.length-1; i>=0; i--) {
+			//System.out.println(sequencia[i]);
+		}
+		
+		int alteracoes = 0;
+		boolean continuar = true;
+		
+		while (continuar == true) {
+			
+			// Ordenar vector decrescente
+			for(int i = 0; i < sequencia.length - 1; i++) {
+				//System.out.println(sequencia[i]);
+				if (sequencia[i] > sequencia[i+1]) {
+					int temp = sequencia[i];
+					
+					sequencia[i] = sequencia[i+1];
+					sequencia[i+1] = temp;
+					
+					alteracoes++;
+				}
+			}
+			
+			if (alteracoes == 0) {
+				continuar = false;
+			}
+			
+			alteracoes = 0;
+			
+		}
+		
+		
+		
+		
+		
+		//mostrar ordenacao
+		for (int i : sequencia) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
